c084e11287f2acde883f5103592de412e6795e96
renatobicego <renatobicego@gmail.com>
2024-08-26 22:57:30 -0300

    Modify middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -10,28 +10,28 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware((auth, req: NextRequest) => {
   const { userId, sessionClaims, redirectToSignIn } = auth();
-  return NextResponse.next();
+  // return NextResponse.next();
 
-  // // If the user isn't signed in and the route is private, redirect to sign-in
-  // if (!userId && !isPublicRoute(req)) {
-  //   return redirectToSignIn({ returnBackUrl: req.url });
-  // }
+  // If the user isn't signed in and the route is private, redirect to sign-in
+  if (!userId && !isPublicRoute(req)) {
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
 
-  // // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-  // // Redirect them to the /onboading route to complete onboarding
-  // if (
-  //   userId &&
-  //   !sessionClaims?.metadata?.onboardingComplete &&
-  //   req.nextUrl.pathname !== "/onboarding"
-  // ) {
-  //   const onboardingUrl = new URL("/onboarding", req.url);
-  //   return NextResponse.redirect(onboardingUrl);
-  // }
+  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+  // Redirect them to the /onboading route to complete onboarding
+  if (
+    userId &&
+    !sessionClaims?.metadata?.onboardingComplete &&
+    req.nextUrl.pathname !== "/onboarding"
+  ) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
 
-  // // If the user is logged in and the route is protected, let them view.
-  // if (userId && !isPublicRoute(req)) {
-  //   return NextResponse.next();
-  // }
+  // If the user is logged in and the route is protected, let them view.
+  if (userId && !isPublicRoute(req)) {
+    return NextResponse.next();
+  }
 });
 
 export const config = {
