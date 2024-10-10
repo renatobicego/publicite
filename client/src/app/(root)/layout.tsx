796d7141edef20338f117c3a69d864c48f641b32
renatobicego <renatobicego@gmail.com>
2024-10-09 21:18:53 -0300

    test without api

--- client/src/app/(root)/layout.tsx
+++ client/src/app/(root)/layout.tsx
@@ -4,9 +4,9 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import { BackgroundProvider } from "./backgroundProvider";
 import BackgroundStyle from "./BackgroundStyle.";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { extractRouterConfig } from "uploadthing/server";
-import { ourFileRouter } from "@/app/api/uploadthing/core"
+// import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+// import { extractRouterConfig } from "uploadthing/server";
+// import { ourFileRouter } from "@/app/api/uploadthing/core"
 export default async function NavigationLayout({
   children,
 }: {
@@ -15,15 +15,9 @@ export default async function NavigationLayout({
   const user = await currentUser();
   return (
     <>
-      <NextSSRPlugin
-        /**
-         * The `extractRouterConfig` will extract **only** the route configs
-         * from the router to prevent additional information from being
-         * leaked to the client. The data passed to the client is the same
-         * as if you were to fetch `/api/uploadthing` directly.
-         */
+      {/* <NextSSRPlugin
         routerConfig={extractRouterConfig(ourFileRouter)}
-      />
+      /> */}
       <Header />
       <BackgroundProvider username={user?.username}>
         <BackgroundStyle />
