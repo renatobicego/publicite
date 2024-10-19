a28c8de9e8aa79a26db2392c885a4a52dab63238
renatobicego <renatobicego@gmail.com>
2024-10-19 16:16:44 -0300

    comment ws

--- client/src/components/Header/Notifications/Notifications.tsx
+++ client/src/components/Header/Notifications/Notifications.tsx
@@ -8,24 +8,24 @@ import { socketUrl } from "@/utils/data/urls";
 const Notifications = () => {
   const [screenSize, setScreenSize] = useState(0);
   const { user } = useUser();
-  const [socket, setSocket] = useState<any>(null);
+  // const [socket, setSocket] = useState<any>(null);
 
-  useEffect(() => {
-    if (user) {
-      // Initialize Socket.IO connection
-      const socketInstance = io(socketUrl, {
-        query: { userId: user?.publicMetadata.mongoId }, // Pass user ID as query parameter
-        transports: ['websocket'], // Ensure only WebSocket is used
-      });
+  // useEffect(() => {
+  //   if (user) {
+  //     // Initialize Socket.IO connection
+  //     const socketInstance = io(socketUrl, {
+  //       query: { userId: user?.publicMetadata.mongoId }, // Pass user ID as query parameter
+  //       transports: ['websocket'], // Ensure only WebSocket is used
+  //     });
 
-      setSocket(socketInstance);
+  //     setSocket(socketInstance);
 
-      // Clean up on unmount
-      return () => {
-        socketInstance.disconnect();
-      };
-    }
-  }, [user]);
+  //     // Clean up on unmount
+  //     return () => {
+  //       socketInstance.disconnect();
+  //     };
+  //   }
+  // }, [user]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -44,21 +44,21 @@ const Notifications = () => {
   }, []);
 
   // Log socket connection status
-  useEffect(() => {
-    if (socket) {
-      socket.on("connect", () => {
-        console.log("Socket connected:", socket.id);
-      });
+  // useEffect(() => {
+  //   if (socket) {
+  //     socket.on("connect", () => {
+  //       console.log("Socket connected:", socket.id);
+  //     });
 
-      socket.on("disconnect", () => {
-        console.log("Socket disconnected");
-      });
+  //     socket.on("disconnect", () => {
+  //       console.log("Socket disconnected");
+  //     });
 
-      socket.on("connect_error", (err: any) => {
-        console.log("Socket connection error:", err);
-      });
-    }
-  }, [socket]);
+  //     socket.on("connect_error", (err: any) => {
+  //       console.log("Socket connection error:", err);
+  //     });
+  //   }
+  // }, [socket]);
 
   return (
     <>
