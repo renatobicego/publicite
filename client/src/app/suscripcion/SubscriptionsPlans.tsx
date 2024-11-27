de7ef08f312d5dfe02461544c877df902dfc47b4
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-11-27 19:27:48 -0300

    fix board update

--- client/src/app/suscripcion/SubscriptionsPlans.tsx
+++ client/src/app/suscripcion/SubscriptionsPlans.tsx
@@ -3,8 +3,8 @@ import { Button, Link } from "@nextui-org/react";
 import { getSubscriptionsPlans, getSubscriptionsPlansMP } from "../../services/subscriptionServices";
 
 const SubscriptionsPlans = async () => {
-  const results = await getSubscriptionsPlans();
-  // const {results} = await getSubscriptionsPlansMP()
+  //const results = await getSubscriptionsPlans();
+  const {results} = await getSubscriptionsPlansMP()
   return <div>
     <h1>Subscriptions Plans</h1>
     <ul>
