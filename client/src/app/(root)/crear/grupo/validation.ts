4c8461a1515bc4311f607cc2712c60c65dee8286
renatobicego <renatobicego@gmail.com>
2025-03-18 22:12:16 -0300

    add max form validation titles

--- client/src/app/(root)/crear/grupo/validation.ts
+++ client/src/app/(root)/crear/grupo/validation.ts
@@ -3,10 +3,12 @@ import { array, number, object, string } from "yup";
 export const groupValidation = object({
   name: string()
     .required("El nombre del grupo es requerido")
-    .min(2, "El nombre del grupo debe tener al menos 2 caracteres"),
+    .min(2, "El nombre del grupo debe tener al menos 2 caracteres")
+    .max(80, "El nombre del grupo debe tener menos de 80 caracteres"),
   alias: string()
     .required("El alias del grupo es requerido")
-    .min(2, "El alias del grupo debe tener al menos 2 caracteres"),
+    .min(2, "El alias del grupo debe tener al menos 2 caracteres")
+    .max(30, "El alias del grupo debe tener menos de 30 caracteres"),
   members: array(string())
     .required("Los miembros son obligatorios")
     .min(1, "Agregue al menos un miembro"),
@@ -21,10 +23,12 @@ export const groupValidation = object({
 export const groupEditValidation = object({
   name: string()
     .required("El nombre del grupo es requerido")
-    .min(2, "El nombre del grupo debe tener al menos 2 caracteres"),
+    .min(2, "El nombre del grupo debe tener al menos 2 caracteres")
+    .max(80, "El nombre del grupo debe tener menos de 80 caracteres"),
   alias: string()
     .required("El alias del grupo es requerido")
-    .min(2, "El alias del grupo debe tener al menos 2 caracteres"),
+    .min(2, "El alias del grupo debe tener al menos 2 caracteres")
+    .max(30, "El alias del grupo debe tener menos de 30 caracteres"),
   details: string()
     .optional()
     .max(200, "La descripción debe tener menos de 200 caracteres"),
