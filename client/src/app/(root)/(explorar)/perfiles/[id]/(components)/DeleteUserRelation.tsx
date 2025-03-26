48a968a6733f38f31696d75cb4d7ae86d76bf6d7
renatobicego <renatobicego@gmail.com>
2025-03-25 21:19:20 -0300

    update active rel on delete

--- client/src/app/(root)/(explorar)/perfiles/[id]/(components)/DeleteUserRelation.tsx
+++ client/src/app/(root)/(explorar)/perfiles/[id]/(components)/DeleteUserRelation.tsx
@@ -1,5 +1,8 @@
 "use client";
+import { setActiveRelations } from "@/app/(root)/providers/slices/configSlice";
+import { useConfigData } from "@/app/(root)/providers/userDataProvider";
 import ConfirmModal from "@/components/modals/ConfirmModal";
+import { getActiveRelations } from "@/services/postsServices";
 import { deleteUserRelation } from "@/services/userServices";
 import { toastifyError, toastifySuccess } from "@/utils/functions/toastify";
 import { Button } from "@nextui-org/react";
@@ -7,21 +10,32 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { IoTrashOutline } from "react-icons/io5";
 
-const DeleteUserRelation = ({relationId} : {relationId: string}) => {
+const DeleteUserRelation = ({ relationId }: { relationId: string }) => {
+  const { updateActiveRelations } = useConfigData();
   const router = useRouter();
-    const handleDelete = async () => {
+  const handleDelete = async () => {
     const res = await deleteUserRelation(relationId);
     if (res.error) {
       toastifyError(res.error);
       return;
     }
     toastifySuccess(res.message || "Relación eliminada");
+    const newActiveRelations = await getActiveRelations();
+    if (!("error" in newActiveRelations)) {
+      updateActiveRelations(newActiveRelations);
+    }
     router.refresh();
   };
   return (
     <ConfirmModal
       ButtonAction={
-        <Button isIconOnly aria-label="Eliminar Relación" color="danger" radius="full" variant="flat">
+        <Button
+          isIconOnly
+          aria-label="Eliminar Relación"
+          color="danger"
+          radius="full"
+          variant="flat"
+        >
           <IoTrashOutline className="size-4" />
         </Button>
       }
