575660011b889947ebbcba501899bcaa10cf3039
renatobicego <renatobicego@gmail.com>
2025-03-30 18:25:08 -0300

    refresh page save post magazine in pagazine page

--- server/src/contexts/module_socket/application/adapter/socket.adapter.interface.ts
+++ server/src/contexts/module_socket/application/adapter/socket.adapter.interface.ts
@@ -1,10 +1,24 @@
 export interface SocketAdapterInterface {
-    sendGroupNotificationToNotificationService(notificationBody: any): Promise<void>;
-    sendMagazineNotificationToNotificationService(notificationBody: any): Promise<void>;
-    sendUserNotificationToNotificationService(notificationBody: any): Promise<void>;
-    sendPostNotificationToNotificationService(notificationBody: any): Promise<void>
-    //sendSubscriptionNotificationToNotificationService(notificationBody:any): Promise<void>
-    sendContactSellerNotificationToNotificationService(notificationBody: any): Promise<void>
-    sendPostCalificationNotificationToNotificationService(notificationBody: any): Promise<void>
-    sendShareNotificationToNotificationService(notificationBody: any): Promise<void>
+  sendGroupNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  sendMagazineNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  sendUserNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  sendPostNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  //sendSubscriptionNotificationToNotificationService(notificationBody:any): Promise<void>
+  sendContactSellerNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  sendPostCalificationNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
+  sendShareNotificationToNotificationService(
+    notificationBody: any,
+  ): Promise<void>;
 }
