2016e49182f935802cc0ee3fc033509fcc59270f
renatobicego <renatobicego@gmail.com>
2025-04-16 21:19:42 -0300

    fix errores

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -9,16 +9,19 @@ export default function Home() {
       id="home-grids"
       className="flex min-h-screen flex-col items-start main-style gap-8"
     >
-      <p className="text-xs lg:text-sm lg:max-w-[50%]">
-        Hola! Estamos trabajando para llevar el sitio al próximo nivel. Queremos
-        mejorar la experiencia de los usuarios! Vamos por uno internacional.
-        Mientras tanto puedes disfrutar del plan gratuito-extendido. Comparte
-        Publicité a tus amigos, y a quien quieras ! Acompáñanos a mejorar el
-        sitio. <strong className="font-noto">Publicité</strong> es tu sitio
-        publicitario.
-      </p>
+      <div className="text-xs lg:text-sm lg:max-w-[50%]">
+        <p>¡Hola! Ayudas para entender el sitio</p>
+        <ul className="list-disc list-inside">
+          <li>Regístrate - Inicia sesión</li>
+          <li>
+            Crea Anuncios - Libres son públicos, de Agenda solo contactos de
+            agenda
+          </li>
+          <li>Crea relaciones</li>
+          <li>Activa cuentas para ver anuncios de tus contactos</li>
+        </ul>
+      </div>
       <SelectManualLocationModal />
-
       <Suspense fallback={<Spinner color="warning" />}>
         <HomePostSection
           type={"goodService"}
@@ -27,7 +30,6 @@ export default function Home() {
           buttonHref={POSTS}
         />
       </Suspense>
-
       <Suspense fallback={<Spinner color="warning" />}>
         <HomePostSection
           type="petition"
