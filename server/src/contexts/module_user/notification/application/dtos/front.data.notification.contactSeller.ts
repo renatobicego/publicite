ded67076da299df6df374667d30410fb3f68329c
renatobicego <renatobicego@gmail.com>
2025-04-22 11:04:58 -0300

    fix notification contact seller

--- server/src/contexts/module_user/notification/application/dtos/front.data.notification.contactSeller.ts
+++ server/src/contexts/module_user/notification/application/dtos/front.data.notification.contactSeller.ts
@@ -1,69 +1,64 @@
-import { ObjectType, Field, Int } from "@nestjs/graphql";
+import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class PostContactSeller {
-    @Field(() => String)
-    _id: string;
+  @Field(() => String)
+  _id: string;
 
-    @Field(() => String)
-    title: string;
+  @Field(() => String)
+  title: string;
 
-    @Field(() => String)
-    description: string;
+  @Field(() => String)
+  description: string;
 
+  @Field(() => String)
+  postType: string;
 
-    @Field(() => String)
-    postType: string;
+  @Field(() => String)
+  price: string;
 
-    @Field(() => String)
-    price: string;
+  @Field(() => [String], { nullable: true })
+  imagesUrls: string[];
 
-    @Field(() => [String])
-    imagesUrls: string[];
+  @Field(() => String, { nullable: true })
+  petitionType: string;
 
-    @Field(() => String, { nullable: true })
-    petitionType: string;
+  @Field(() => String, { nullable: true })
+  toPrice: string;
 
-
-    @Field(() => String, { nullable: true })
-    toPrice: string;
-
-
-    @Field(() => String, { nullable: true })
-    frequencyPrice: string;
-
-
+  @Field(() => String, { nullable: true })
+  frequencyPrice: string;
 }
 
 @ObjectType()
 export class ClientContactSeller {
-    @Field(() => String, { nullable: true })
-    _id: string;
+  @Field(() => String, { nullable: true })
+  _id: string;
 
-    @Field(() => String)
-    name: string;
+  @Field(() => String)
+  name: string;
 
-    @Field(() => String)
-    email: string;
+  @Field(() => String)
+  email: string;
 
-    @Field(() => String)
-    lastName: string;
+  @Field(() => String)
+  lastName: string;
 
-    @Field(() => String, { nullable: true })
-    username: string;
+  @Field(() => String, { nullable: true })
+  username: string;
 
-    @Field(() => String, { nullable: true })
-    phone: string;
+  @Field(() => String, { nullable: true })
+  phone: string;
 
-    @Field(() => String)
-    message: string;
+  @Field(() => String)
+  message: string;
 }
 
 @ObjectType()
 export class front_data_CONTACTSELLER {
-    @Field(() => PostContactSeller)
-    post: PostContactSeller;
+  @Field(() => PostContactSeller)
+  post: PostContactSeller;
 
-    @Field(() => ClientContactSeller)
-    client: ClientContactSeller;
+  @Field(() => ClientContactSeller)
+  client: ClientContactSeller;
 }
