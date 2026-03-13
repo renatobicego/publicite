7bc6a15929dd3b2b6c843573b0b7eaaa8022798e
renatobicego <renatobicego@gmail.com>
2026-03-12 23:27:27 -0300

    fix mock data

--- client/src/components/chips/UserAmountChips.tsx
+++ client/src/components/chips/UserAmountChips.tsx
@@ -2,12 +2,14 @@ import { Chip } from "@nextui-org/react";
 import axios from "axios";
 
 const UserAmountChips = async () => {
-  // const { data: userCount }: { data: number } = await axios.get(
-  //   `${process.env.API_URL}/user/count`
-  // );
+  const { data: userCount }: { data: number } = await axios.get(
+    `${process.env.API_URL}/user/count`
+  );
 
   return (
-    <Chip className="mt-2 bg-service text-white">¡{} usuarios activos!</Chip>
+    <Chip className="mt-2 bg-service text-white">
+      ¡{userCount} usuarios activos!
+    </Chip>
   );
 };
 
